dee24372171aa03fce2854a3cbf81ec2ac988a0f
Vincent Turnier <vincentturnier@gmail.com>
2024-05-21 22:39:34 -0400

    fix package name

--- Tests/QuestionnaireTests/QuestionnaireTests.swift
+++ Tests/QuestionnaireTests/QuestionnaireTests.swift
@@ -1,10 +1,13 @@
 import XCTest
-@testable import sdk
+@testable import Questionnaire
 
 final class sdkTests: XCTestCase {
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
+        let questionnaire = Questionnaire.Builder()
+            .build()
+        
 
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
