5a10a152888150292f58af43f4f11df263a13c64
Vincent Turnier <vincentturnier@gmail.com>
2024-05-22 07:51:06 -0400

    launching ui

--- Sources/Questionnaire/Delegate/QuestionnaireDelegate.swift
+++ Sources/Questionnaire/Delegate/QuestionnaireDelegate.swift
@@ -7,19 +7,19 @@
 
 import Foundation
 //import SwiftUI
-//import UIKit
+import UIKit
 //
-//@UIApplicationMain
-//class QuestionnaireDelegate: UIResponder, UIApplicationDelegate {
-//    
-//    var window: UIWindow?
-//    
-//    func loadNewRootSwiftUIView(rootViewController: UIViewController)
-//        {
-//            let window = UIWindow(frame: UIScreen.main.bounds)
-//            window.rootViewController = rootViewController
-//            self.window = window
-//            window.makeKeyAndVisible()
-//        }
-//    
-//}
+@UIApplicationMain
+class QuestionnaireDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func loadNewRootSwiftUIView(rootViewController: UIViewController)
+        {
+            let window = UIWindow(frame: UIScreen.main.bounds)
+            window.rootViewController = rootViewController
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    
+}
--- Sources/Questionnaire/Questionnaire.swift
+++ Sources/Questionnaire/Questionnaire.swift
@@ -1,7 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-//import UIKit
+import UIKit
 import SwiftUI
 
 public class Questionnaire {
